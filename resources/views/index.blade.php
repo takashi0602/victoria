e658bac848eccea3c422ab6899c176a54ebca5e2
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-28 23:03:14 +0900

    ハロワ

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Victoria</title>
+    </head>
+    <body>
+        <div id="app">
+            hello, world!
+        </div>
+    </body>
+</html>
