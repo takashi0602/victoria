031868fca49e87e48fe3d1b423dc7773dc95b9b2
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-02 16:44:54 +0900

    コントローラ修正

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,9 +9,7 @@ class IndexController extends Controller
     public function search() {
         $tweets = DB::select('SELECT * FROM tweets');
         $tweets = json_encode($tweets);
-        return view('index', [
-            'tweets' => $tweets
-        ]);
+        return $tweets;
     }
 
     /**
