bf382e9b53f18f107173cb3e2ac7a1455ae881e6
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-28 17:54:24 +0900

    マイグレーション修正

--- database/migrations/2017_12_31_135520_create_tweets_table.php
+++ database/migrations/2017_12_31_135520_create_tweets_table.php
@@ -16,7 +16,6 @@ class CreateTweetsTable extends Migration
         Schema::create('tweets', function (Blueprint $table) {
             $table->increments('id');
             $table->text('tweet');
-            $table->dateTime('created_at');
         });
     }
 
