6703a5841d0fef0bbfea335f76b861207e9b6738
takashi <b7180@oic.jp>
2018-02-07 11:04:28 +0900

    Tweet取得しないようにした

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,28 +8,28 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 class IndexController extends Controller
 {
     public function setTweets() {
-        $CONSUMER_KEY = env('CONSUMER_KEY');
-        $CONSUMER_SECRET = env('CONSUMER_SECRET');
-        $ACCESS_TOKEN = env('ACCESS_TOKEN');
-        $ACCESS_TOKEN_SECRET = env('ACCESS_TOKEN_SECRET');
-
-        $SEARCH_KEYWORD = env('SEARCH_KEYWORD');
-
-        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
-
-        $response = $connection->get('search/tweets', [
-            'q' => '#' . $SEARCH_KEYWORD . ' -RT',
-            'count' => '30',
-            'result_type' => 'recent'
-        ]);
-
-        for ($i = 0; $i < 29; $i++) {
-            DB::table('tweets')->insert([
-                'tweet' => $response->statuses[$i]->text,
-            ]);
-        }
-
-        return;
+//        $CONSUMER_KEY = env('CONSUMER_KEY');
+//        $CONSUMER_SECRET = env('CONSUMER_SECRET');
+//        $ACCESS_TOKEN = env('ACCESS_TOKEN');
+//        $ACCESS_TOKEN_SECRET = env('ACCESS_TOKEN_SECRET');
+//
+//        $SEARCH_KEYWORD = env('SEARCH_KEYWORD');
+//
+//        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+//
+//        $response = $connection->get('search/tweets', [
+//            'q' => '#' . $SEARCH_KEYWORD . ' -RT',
+//            'count' => '30',
+//            'result_type' => 'recent'
+//        ]);
+//
+//        for ($i = 0; $i < 29; $i++) {
+//            DB::table('tweets')->insert([
+//                'tweet' => $response->statuses[$i]->text,
+//            ]);
+//        }
+//
+//        return;
     }
 
     public function getTweets() {
